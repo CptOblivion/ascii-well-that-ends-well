b4ff60b6ca71dd80b72d8c4c0a92eaaf93926014
Liam Richardson <liam@richardsonconcepts.com>
2022-01-25 11:32:33 -0500

    Centered art, opened references to hooks

--- client/src/components/GalleryEntry.jsx
+++ client/src/components/GalleryEntry.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
 function GalleryEntry({ entry }) {
+
+  const [fontSize, setFontSize] = useState(10);
+  const ref = useRef();
+
+  useEffect(() => {
+    if (ref.current) {
+      console.log(ref.current)
+    }
+  }, [ref.current])
+
+
   return (
-    <div className='art'>
-      {/* {entry.ascii} */}
-      {entry.ascii.map((line, i) => <div key={i}>{line}</div>)}
+    <div className='art' ref={ref}>
+      <div>
+        {entry.ascii.map((line, i) => <div key={i}>{line}</div>)}
+      </div>
     </div>
   )
 }
