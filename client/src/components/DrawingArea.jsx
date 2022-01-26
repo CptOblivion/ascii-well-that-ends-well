4c8f2ae1e3bc3fbde19486c06548302a005e28c4
Liam Richardson <liam@richardsonconcepts.com>
2022-01-25 22:34:53 -0500

    Non-functional framework for pixel crawl

--- client/src/components/DrawingArea.jsx
+++ client/src/components/DrawingArea.jsx
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import ASCIIDisplay from './ASCIIDisplay.jsx';
 
 
-const artGrid = [80, 40];
-const artPixels = [2, 4];
-const width = artGrid[0] * artPixels[0];
-const height = artGrid[1] * artPixels[1];
-const line = Array(artGrid[0]).fill('.').join('');
-const emptyAscii = Array(artGrid[1]).fill(line).join('\n')
+const [gridWidth, gridHeight] = [80, 40];
+const [blockWidth, blockHeight] = [2, 4];
+const width = gridWidth * blockWidth;
+const height = gridHeight * blockHeight;
+const line = Array(gridWidth).fill('.').join('');
+const emptyAscii = Array(gridHeight).fill(line).join('\n')
 
 function DrawingArea({ updateArt }) {
   const [mouseDown, setMouseDown] = useState(false);
@@ -28,25 +28,34 @@ function DrawingArea({ updateArt }) {
       ctx.moveTo((endX - e.movementX) * scaleX, (endY - e.movementY) * scaleY);
       ctx.lineTo(endX * scaleX, endY * scaleY);
       ctx.stroke();
-      imageGrid()
+      imageGrid(ctx.getImageData(0, 0, width, height).data)
     }
   }
 
-  function imageGrid() {
+  function imageGrid(pixelArray) {
     /**
      * break image pixels into blocks
      * take advantage of linear nature of pixel array, and linear nature of a string:
      * walk through string (skip newlines), get segment multiplying i by image width, then use segment length, repeat segment height times
      */
-
     let newAscii = '';
-    for (let i = 0; i < artGrid[1] + 1; i++) {
-      for (let j = 0; j < artGrid[0] + 1; j++) {
-        if (j === artGrid[0]) {
-          newAscii += '\n'
-        } else {
-          newAscii += '/'
+    for (let i = 0; i < gridHeight; i++) {
+      for (let j = 0; j < gridWidth; j++) {
+        const blockOffset = i * gridHeight * blockHeight * 4
+        // const block = []
+        for (let bi = 0; bi < height; bi += width * 4)  {
+          for(let bj = blockOffset + bi; bj < blockOffset + bi + blockWidth; bj += 4) {
+            if (pixelArray[bj] > 128) {
+              console.log(bi, bj)
+              newAscii += '@'
+              break
+            }
+          }
+          // block.push(pixelArray.slice(, blockOffset + bi + blockSize[0]))
         }
+        newAscii += '.'
+        //end of the line
+        if (j === gridWidth - 1) newAscii += '\n'
       }
     }
     setAscii(newAscii);
@@ -65,7 +74,7 @@ function DrawingArea({ updateArt }) {
         style={{ width: '100%', height: 'auto' }}
       />
       <div style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0, pointerEvents: 'none' }}>
-        <ASCIIDisplay entry={{ ascii: ascii, asciiLines: ascii.split('\n'), asciiWidth: artGrid[0] }} size={800} />
+        <ASCIIDisplay entry={{ ascii: ascii, asciiLines: ascii.split('\n'), asciiWidth: gridWidth }} size={800} />
       </div>
     </div>
   );
